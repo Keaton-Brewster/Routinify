5ecf63dc2443a518cfcf2ea5df4ee9160160933e
Justin Yates <justinyates887@gmail.com>
2021-03-20 08:28:39 -0700

    setup server

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
-// This is where our code will go!
+const express = require('express');
 
-const app = require('express');
+const port = process.env.PORT || 8080
+var db = require("./models");
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use(session({ secret: "secret", resave: true, saveUninitialized: true }));
+
+//routes
+
+db.sequelize.sync().then(function() {
+    app.listen(PORT, function() {
+      console.log(`Listening on port ${port}.`, PORT, PORT);
+    });
+  });
+  
