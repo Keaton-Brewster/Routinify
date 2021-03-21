df0416f86dff49e300ff464314f37975739cc888
Delta9k8b <keaton.brewster@gmail.com>
2021-03-21 16:49:42 -0500

    removing unneded dependencies

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 8080;
 const db = require('./models');
 
 const app = express();
-app.use(bodyParser.urlencoded({
+app.use(express.urlencoded({
   extended: true
 }));
-app.use(cookieParser());
 
 app.use(express.json());
 app.use(express.static('public'));
@@ -27,11 +24,11 @@ app.use(session({
 
 
 db.sequelize.sync().then(() => {
-    try{
-        app.listen(PORT, () => {
-            console.log(`Listening on port ${PORT}.`, PORT, PORT);
-        });
-    } catch(err){
-        console.error(`Error at server.js(35): ${err}`)
-    }
+  try {
+    app.listen(PORT, () => {
+      console.log(`Listening on port ${PORT}.`, PORT, PORT);
+    });
+  } catch (err) {
+    console.error(`Error at server.js(35): ${err}`);
+  }
 });
