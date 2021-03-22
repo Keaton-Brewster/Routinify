95c6afe247ee424bddf2e7d1a8a218f8b135c13c
Maggie may <maggiemaywilder@gmail.com>
2021-03-21 21:57:32 -0500

    rough create function for signup functionality

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,4 +1,5 @@
 const db = require('../models');
+const user = require('../models/user');
 
 // routes for when there's not a user logged in
 module.exports = function(app) {
@@ -10,8 +11,17 @@ module.exports = function(app) {
 
     // create new user (need middleware before (req, res) to check for existing user)
     app.post('/api/signup', (req, res) => {
-
-    })
+        // need value from input.trim() username & hashed password from listeners in public/assets/js
+        // if signup is always admin, and users can only be added from admin being logged in, isAdmin =  true
+        // need variable for username
+        const username = await User.create({
+            username: 'req.userName',
+            email: 'req.email',
+            password: 'req.password',
+            isAdmin: true,
+        });
+        console.log('New user ID: ', username.id);
+    });
 
     // login (need middleware before (req, res))
     app.post('/api/login', (req, res) => {
