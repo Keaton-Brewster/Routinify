cf7472680ce5970d19eb1a94779e0c9fe4c7887b
Delta9k8b <keaton.brewster@gmail.com>
2021-03-22 06:18:27 -0500

    updated server file

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+const passport = require('./config/passport.js');
 
 const PORT = process.env.PORT || 8080;
 const db = require('./models');
@@ -8,24 +11,30 @@ const app = express();
 app.use(express.urlencoded({
   extended: true
 }));
-
-
 app.use(express.json());
 app.use(express.static('public'));
 
 app.use(session({
-  secret: 'secret',
+  secret: 'the family whiteboard',
   resave: true,
   saveUninitialized: true
 }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//set up handlebars
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
 
 //routes
-require('./routes/api-routes')(app);
-require('./routes/html-routes')(app);
+// require('./controller/api_controller')(app);
+// require('./controller/html_controller')(app);
 
 
 db.sequelize.sync().then(() => {
   app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}.`, PORT, PORT);
+    console.log('Listening on port', PORT);
   });
 });
