d7535a8557d3d956d6ef861de1183575c0d53545
Delta9k8b <keaton.brewster@gmail.com>
2021-03-22 05:59:02 -0500

    linting

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,19 +2,19 @@ const db = require('../models');
 const user = require('../models/user');
 
 // routes for when there's not a user logged in
-module.exports = function(app) {
-    
+module.exports = function (app) {
+
     // load home screen
     app.get('/', (req, res) => {
-        
-    })
+
+    });
 
     // create new user (need middleware before (req, res) to check for existing user)
-    app.post('/api/signup', (req, res) => {
+    app.post('/api/signup', async (req, res) => {
         // need value from input.trim() username & hashed password from listeners in public/assets/js
         // if signup is always admin, and users can only be added from admin being logged in, isAdmin =  true
         // need variable for username
-        const username = await User.create({
+        const username = await db.User.create({
             username: 'req.userName',
             email: 'req.email',
             password: 'req.password',
@@ -26,7 +26,7 @@ module.exports = function(app) {
     // login (need middleware before (req, res))
     app.post('/api/login', (req, res) => {
 
-    })
+    });
 
 
-}
+};
