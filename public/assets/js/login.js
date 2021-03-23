43135e486e514a96d32bb5deda60781bbd70d4f5
Delta9k8b <keaton.brewster@gmail.com>
2021-03-23 08:52:24 -0500

    still just trying to make sense of headers?

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,26 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 
-    const login = (user) => {
-        // return new Promise((resolve, reject) => {
-        $.post('/api/login', user);
-            // .then(() => {
-            //     // const {
-            //     //     id
-            //     // } = userData;
-            //     // if (id) {
-            //     //     resolve(id);
-            //     // } else {
-            //     //     reject('something is off');
-            //     // }
-            //     // });
-            //     // window.location.replace('/users/home');
-            // });
-    };
+    // const login = (user) => {
+    //     // return new Promise((resolve, reject) => {
+    //     $.post('/api/login', user);
+    //         // .then(() => {
+    //         //     // const {
+    //         //     //     id
+    //         //     // } = userData;
+    //         //     // if (id) {
+    //         //     //     resolve(id);
+    //         //     // } else {
+    //         //     //     reject('something is off');
+    //         //     // }
+    //         //     // });
+    //         //     // window.location.replace('/users/home');
+    //         // });
+    // };
 
 
 
-    document.querySelector('#login').addEventListener('submit', async () => {
+    document.querySelector('#login_button').addEventListener('click', () => {
         const userEmail = $('input#user').val();
         const userPassword = $('input#password').val();
 
@@ -31,7 +31,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
         if (userData.email && userData.password) {
 
-            login(userData);
+            $.post('/api/login', userData);
+            // login(userData);
             // login(userData).then(id => {
             //     $.get(`/users/home/${id}`)
             //         .catch(error => console.error(error));
