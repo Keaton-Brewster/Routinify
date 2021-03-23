72ad4b8501a1a78854b31e5f6e07074da6f95924
Delta9k8b <keaton.brewster@gmail.com>
2021-03-22 19:23:54 -0500

    commenting

--- db/schema.sql
+++ db/schema.sql
@@ -25,3 +25,39 @@ CREATE TABLE messages (
     body TEXT NOT NULL,
     isRead BOOLEAN NOT NULL
 );
+/* below if what data will look like (data being req.user)
+ {
+ "id": 2,
+ "username": "foo",
+ "email": "foo@bar.fo",
+ "password": "$2a$10$Jx9EDvrKpxzxdSkqT0e14escXpdVb6kjq0JKTps/CmDD0SMmLK8Du",
+ "isAdmin": false,
+ "createdAt": "2021-03-22T22:59:47.000Z",
+ "updatedAt": "2021-03-22T22:59:47.000Z"
+ add "groups": [];
+ }
+ 
+ How we want the group to look: 
+ {
+ "id": 1,
+ "group": "group name",
+ "routines": [foreign keys relating to routines?],
+ "owner": foreignKey(userID),
+ }
+ 
+ How routines need to look 
+ {
+ "id": autoIncr,
+ "routine": "routine name"
+ "tasks": [task IDs],
+ "assignees": [user IDs],
+ "complete": boolean
+ }
+ 
+ How tasks need to look 
+ {
+ "id": autoIncr,
+ "task": "take out trash"
+ "complete": boolean
+ }
+ */
