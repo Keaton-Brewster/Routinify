f39a7e7643b8134160766605b9b07a6485f9b993
Delta9k8b <keaton.brewster@gmail.com>
2021-03-24 20:00:57 -0500

    added boilerplate functionality for the add user to group stuff

---
+++ public/assets/js/group_page.js
@@ -0,0 +1,21 @@
+$(document).ready(() => {
+    const addUserForm = $('#addUserForm');
+
+    addUserForm.on('submit', (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        let username = $('#addUser').val();
+        let groupId = $('#addUserButton').attr('data-id');
+
+        console.log(username, groupId);
+
+        username = encodeURIComponent(username);
+        groupId = encodeURIComponent(groupId);
+
+        $.post(`/api/groups/add_user_by_username/?user=${username}&group=${groupId}`)
+            .catch(error => {
+                console.log(error);
+            });
+    });
+});
