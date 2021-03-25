d7a29bd69fcff48215c3b2ed418401f97d39011f
Delta9k8b <keaton.brewster@gmail.com>
2021-03-24 20:42:35 -0500

    efffed up server lols

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 
 const passport = require('./config/passport.js');
-
-<<<<<<< HEAD
 const PORT = process.env.PORT || 8080;
 const db = require('./models');
 
@@ -36,52 +33,9 @@ require('./controller/api_controller')(app);
 require('./controller/html_controller')(app);
 
 db.sequelize.sync().then(() => {
-<<<<<<< HEAD
   app.listen(PORT, () => {
     console.log('Listening on port', PORT);
   });
-=======
-const app = require('express');
-
-const exphbs = require("express-handlebars");
-
-var PORT = process.env.PORT || 8080;
-
-var apps = express();
-
-// Serve static content for the apps from the "public" directory in the application directory.
-// This is a level of abstraction to hide credentials from user
-apps.use(express.static('public'));
-
-// Parse application body as JSON
-apps.use(express.urlencoded({
-  extended: false
-}));
-apps.use(express.json());
-
-// the main page is always displayed
-
-apps.engine("handlebars", exphbs({
-  defaultLayout: "main"
-}));
-apps.set("view engine", "handlebars");
-
-// Import routes and give the server access to them.
-// Norm is to call it "var = routes"
-var routes = require("./controllers.js");
-
-// Use express routes defined
-apps.use("/", routes);
-
-// Start our server so that it can begin listening to client requests.
-apps.listen(PORT, function () {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
->>>>>>> ac94b36be12aa33bda85a1a111333437ab9ba595
-});
-=======
->>>>>>> 23a3805cf0b739d6720ff8e002c6cbea8fb824fd
-=======
   try {
     app.listen(PORT, () => {
       console.log(`Listening on port ${PORT}.`);
@@ -90,4 +44,3 @@ apps.listen(PORT, function () {
     console.error(`Error at server.js(35): ${err}`);
   }
 });
->>>>>>> development
